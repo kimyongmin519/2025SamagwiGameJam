780ba2e1a4f5e603028512c792851aac53ad0810
dotchan-IYC <imyeachan1224@gmail.com>
2025-12-22 19:11:51 +0900

    feaaat
    feat

--- 2025GameJamTest/Assets/Member/IYC/Code/Gimic/infinitygameobject.cs
+++ 2025GameJamTest/Assets/Member/IYC/Code/Gimic/infinitygameobject.cs
@@ -5,11 +5,11 @@ public class infinitygameobject : MonoBehaviour
 {
     [SerializeField]
     private Player player;
-
+    [SerializeField] private Transform[] backgrounds;
 
     private void Update()
     {
-        if(player.gameObject.transform.position.x == gameObject.transform.position.x + 3.11)
+        if(player.gameObject.transform.position.x == backgrounds[2].gameObject.transform.position.x + 3.11)
         {
             
         }
