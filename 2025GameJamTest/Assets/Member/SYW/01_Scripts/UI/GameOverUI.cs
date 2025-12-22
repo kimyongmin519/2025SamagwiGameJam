162d2888737848382cc57480744560e08309aa05
yeonwoo0981 <0981syw@naver.com>
2025-12-22 17:27:33 +0900

    GameOver/Victory UI. fix GameManager

--- 2025GameJamTest/Assets/Member/SYW/01_Scripts/UI/GameOverUI.cs
+++ 2025GameJamTest/Assets/Member/SYW/01_Scripts/UI/GameOverUI.cs
@@ -1,25 +1,11 @@
 using System;
+using Member.SYW._01_Scripts.Manager;
 using UnityEngine;
 
 namespace Member.SYW._01_Scripts.UI
 {
-    public class GameOverUI : MonoBehaviour
+    public class GameOverUI : MonoSingleton<GameOverUI>
     {
-        private GameManager _gameManager;
-
-        private void Awake()
-        {
-            _gameManager = GameManager.Instance;
-        }
-
-        private void OnEnable()
-        {
-            //_gameManager.OnGameOver +=
-        }
-
-        private void OnDisable()
-        {
-            //_gameManager.OnGameOver -=
-        }
+        
     }
 }
--- 2025GameJamTest/Assets/Member/SYW/01_Scripts/UI/GameWinUI.cs
+++ 2025GameJamTest/Assets/Member/SYW/01_Scripts/UI/GameWinUI.cs
@@ -1,24 +1,10 @@
+using Member.SYW._01_Scripts.Manager;
 using UnityEngine;
 
 namespace Member.SYW._01_Scripts.UI
 {
-    public class GameWinUI : MonoBehaviour
+    public class GameWinUI : MonoSingleton<GameWinUI>
     {
-        private GameManager _gameManager;
-
-        private void Awake()
-        {
-            _gameManager = GameManager.Instance;
-        }
-
-        private void OnEnable()
-        {
-            //_gameManager.OnGameOver +=
-        }
-
-        private void OnDisable()
-        {
-            //_gameManager.OnGameOver -=
-        }
+        
     }
 }
