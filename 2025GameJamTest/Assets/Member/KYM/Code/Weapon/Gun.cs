1eaf312e5afc83ae1c5916ea258cf4bfb42327d7
kimyongmin519 <qwdfbns5198@gmail.com>
2025-12-22 13:39:33 +0900

    etc

--- 2025GameJamTest/Assets/Member/KYM/Code/Weapon/Gun.cs
+++ 2025GameJamTest/Assets/Member/KYM/Code/Weapon/Gun.cs
@@ -5,7 +5,17 @@ namespace Member.KYM.Code.Weapon
     public class Gun : MonoBehaviour
     {
         private Vector2 _mouseDir;
-        
+
+        private void Update()
+        {
+            float angle = Mathf.Atan2(_mouseDir.y, _mouseDir.x) * Mathf.Rad2Deg;
+            
+            //transform.rotation = Quaternion.Euler(new Vector3(0,0,))
+        }
+        public void SetMouseDir(Vector2 mouseDir)
+        {
+            _mouseDir = mouseDir;
+        }
         
     }
 }
