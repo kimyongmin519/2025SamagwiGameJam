4df2be492fece351e1dd4f074439cb69879bc85c
yeonwoo0981 <0981syw@naver.com>
2025-12-23 10:46:02 +0900

    fix: Grade Segmentation, Tween Upgrade UI

--- 2025GameJamTest/Assets/Member/SYW/01_Scripts/UI/UpgradeUI.cs
+++ 2025GameJamTest/Assets/Member/SYW/01_Scripts/UI/UpgradeUI.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,34 +6,75 @@ namespace Member.SYW._01_Scripts.UI
 {
     public class UpgradeUI : MonoBehaviour
     {
-        [SerializeField] private Image gunVisual;
+        [Header("UI Elements")]
         [SerializeField] private Image panel1;
         [SerializeField] private Image panel2;
         [SerializeField] private Image panel3;
         [SerializeField] private Image panel4;
+
+        [Header("Settings")]
+        [SerializeField] private float animationDuration = 0.5f;
+        [SerializeField] private float slideOffset = 1000f;
         
+        private Sequence _animSeq;
+        
+        private Vector2 _p1Origin, _p2Origin, _p3Origin, _p4Origin;
+
+        private void Awake()
+        {
+            _p1Origin = panel1.rectTransform.anchoredPosition;
+            _p2Origin = panel2.rectTransform.anchoredPosition;
+            _p3Origin = panel3.rectTransform.anchoredPosition;
+            _p4Origin = panel4.rectTransform.anchoredPosition;
+        }
+
         public void Show()
         {
             gameObject.SetActive(true);
+            
+            panel1.rectTransform.anchoredPosition = _p1Origin + new Vector2(-slideOffset, 0);
+            panel2.rectTransform.anchoredPosition = _p2Origin + new Vector2(-slideOffset, 0);
+            
+            panel3.rectTransform.anchoredPosition = _p3Origin + new Vector2(slideOffset, 0);
+            panel4.rectTransform.anchoredPosition = _p4Origin + new Vector2(slideOffset, 0);
+
+            if (_animSeq != null && _animSeq.IsActive()) _animSeq.Kill();
+            _animSeq = DOTween.Sequence().SetUpdate(true);
+
+            _animSeq.Join(panel1.rectTransform.DOAnchorPos(_p1Origin, animationDuration).SetEase(Ease.OutBack));
+            _animSeq.Join(panel2.rectTransform.DOAnchorPos(_p2Origin, animationDuration).SetEase(Ease.OutBack));
+            _animSeq.Join(panel3.rectTransform.DOAnchorPos(_p3Origin, animationDuration).SetEase(Ease.OutBack));
+            _animSeq.Join(panel4.rectTransform.DOAnchorPos(_p4Origin, animationDuration).SetEase(Ease.OutBack));
+            
             Time.timeScale = 0;
         }
 
         public void Choose()
         {
-            // 여기다가 버프 넘기기
-            StartCoroutine(ChooseCoroutine());
+            // 여기다가 버프 넘기기 쓰면 됨
+            PlayExitAnimation();
+        }
+
+        private void PlayExitAnimation()
+        {
+            if (_animSeq != null && _animSeq.IsActive()) _animSeq.Kill();
+            
+            _animSeq = DOTween.Sequence().SetUpdate(true);
+            
+            _animSeq.Join(panel1.rectTransform.DOAnchorPos(_p1Origin + new Vector2(-slideOffset, 0), animationDuration).SetEase(Ease.InBack));
+            _animSeq.Join(panel2.rectTransform.DOAnchorPos(_p2Origin + new Vector2(-slideOffset, 0), animationDuration).SetEase(Ease.InBack));
+            _animSeq.Join(panel3.rectTransform.DOAnchorPos(_p3Origin + new Vector2(slideOffset, 0), animationDuration).SetEase(Ease.InBack));
+            _animSeq.Join(panel4.rectTransform.DOAnchorPos(_p4Origin + new Vector2(slideOffset, 0), animationDuration).SetEase(Ease.InBack));
+            
+            _animSeq.OnComplete(ChooseComplete);
         }
         
         private void ChooseComplete()
         {
-            gameObject.SetActive(false);
-            Time.timeScale = 1;
-        }
+            if (_animSeq != null) _animSeq.Kill();
 
-        private IEnumerator ChooseCoroutine()
-        {
-            yield return null;
-            ChooseComplete();
+            Time.timeScale = 1;
+            gameObject.SetActive(false);
         }
     }
 }
