dffd0ec4003afa03cd37c42729e2946c55fb29ed
yeonwoo0981 <0981syw@naver.com>
2025-12-23 23:45:03 +0900

    Game Ready

--- 2025GameJamTest/Assets/Member/KYM/Code/LobbySanta.cs
+++ 2025GameJamTest/Assets/Member/KYM/Code/LobbySanta.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using TransitionsPlus;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,13 +7,15 @@ namespace Member.KYM.Code
 {
     public class LobbySanta : MonoBehaviour
     {
+        [SerializeField] private TransitionAnimator transition;
+        
         public void Move(Transform goal)
         {
             Sequence s = DOTween.Sequence();
             
             s.Append(transform.DOMove(goal.position, 0.1f).SetEase(Ease.Linear));
             s.AppendInterval(1f);
-            s.AppendCallback(() => SceneManager.LoadScene("InGame"));
+            s.AppendCallback(() => transition.gameObject.SetActive(true));
         }
     }
 }
