77bc69834b93aa595d1ed891c3319cb4390c0268
yeonwoo0981 <0981syw@naver.com>
2025-12-23 09:56:10 +0900

    Ex: Upgrade System, UI

--- 2025GameJamTest/Assets/Member/SYW/01_Scripts/UI/UpgradeUI.cs
+++ 2025GameJamTest/Assets/Member/SYW/01_Scripts/UI/UpgradeUI.cs
@@ -1,9 +1,40 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Member.SYW._01_Scripts.UI
 {
     public class UpgradeUI : MonoBehaviour
     {
+        [SerializeField] private Image gunVisual;
+        [SerializeField] private Image panel1;
+        [SerializeField] private Image panel2;
+        [SerializeField] private Image panel3;
+        [SerializeField] private Image panel4;
         
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        public void Choose()
+        {
+            // 여기다가 버프 넘기기
+            StartCoroutine(ChooseCoroutine());
+        }
+        
+        private void ChooseComplete()
+        {
+            gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+        private IEnumerator ChooseCoroutine()
+        {
+            yield return null;
+            ChooseComplete();
+        }
     }
 }
